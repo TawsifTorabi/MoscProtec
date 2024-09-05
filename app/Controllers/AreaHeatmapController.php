6f076aa9ceec82cdcf5e853c496bbe701d86d2c2
tawsiftorabi <tawsiftorabi.jr@gmail.com>
2024-09-05 22:51:57 +0600

    Setting Heatmap Pages

--- app/Controllers/AreaHeatmapController.php
+++ app/Controllers/AreaHeatmapController.php
@@ -10,8 +10,12 @@ class AreaHeatmapController extends BaseController
     //////////////////////////////////////////////Dashboard/////////////////////////////////////////////////
     public function index(){
         $data['title'] = 'Home Page';
-        echo view('includes\user\AreaHeatMap\HeatMapMain.php', $data);
+        echo view('includes\user\AreaHeatMap\HeatMap_Header.php', $data);
         echo view('includes\user\AreaHeatMap\HeatMap_Head_Assets.php');
+        echo view('includes\user\AreaHeatMap\HeatMap_Navigation.php');
+        echo view('includes\user\AreaHeatMap\HeatMap_Main_Layout.php');
+        echo view('includes\user\AreaHeatMap\HeatMap_Footer.php');
+
     }
 
         
--- app/Views/includes/user/AreaHeatMap/HeatMap_Footer.php
+++ app/Views/includes/user/AreaHeatMap/HeatMap_Footer.php
@@ -45,15 +45,6 @@
     </div>
     <!-- / Layout wrapper -->
 
-    <div class="buy-now">
-      <a
-        href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
-        target="_blank"
-        class="btn btn-danger btn-buy-now"
-        >Upgrade to Pro</a
-      >
-    </div>
-
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
     <script src="../assets/vendor/libs/jquery/jquery.js"></script>
@@ -73,7 +64,5 @@
     <!-- Page JS -->
     <script src="../assets/js/dashboards-analytics.js"></script>
 
-    <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
   </body>
 </html>
